ddde5950f1fe024e861f429bb84e106a88ba96d3
asha-0321 <ashadevikasa141@gmail.com>
2024-07-10 19:34:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import streamlit as st                                                  
-from gtts import gTTS                                                   
+import streamlit as st                                                                                                 
 import os                                                               
 import base64 
 
